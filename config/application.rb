50108c9fd1b298db64543127eb08d2cad06d329a
Thomas <twkeating1@gmail.com>
2017-02-10 03:51:04 +0000

    Commit before layout work

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Workspace
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
+    #the line above was giving me a deprecation warning so I commented it out
   end
 end
