809f06d71814a0d9a7ba7f3b564df9cf5a59665b
Thomas <twkeating1@gmail.com>
2017-02-16 03:15:02 +0000

    Probably a Good place to stop

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170213172138) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "remember_digest"
+    t.boolean  "admin"
+    t.string   "activation_digest"
+    t.boolean  "activated"
+    t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 
 end
