a4fe5dc48add210de5bd79f130f81da3e15a6ac7
Thomas <twkeating1@gmail.com>
2017-02-18 00:40:34 +0000

    following and feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  has_many: microposts, dependent: :destroy
-  attr_accessor :remember_token, :activation_token
+  has_many :microposts, dependent: :destroy
+  attr_accessor :remember_token, :activation_token, :reset_token
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
@@ -10,14 +19,15 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
                   
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-  # Returns a random token. This is also a class method??? 
+  
+  
   def User.new_token
     SecureRandom.urlsafe_base64
   end
@@ -65,11 +75,10 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
   
-=begin 
   def feed
-     following_ids = "SELECT followed_id FROM relationships
+      following_ids = "SELECT followed_id FROM relationships
                      WHERE  follower_id = :user_id"
-    Micropost.where("user_id IN (#{following_ids})
+      Micropost.where("user_id IN (#{following_ids})
                      OR user_id = :user_id", user_id: id)
   end
   
@@ -85,7 +94,7 @@ class User < ApplicationRecord
   def following?(other_user)
     following.include?(other_user)
   end
-=end
+
 
   private 
   
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Workspace
     # Do not swallow errors in after_commit/after_rollback callbacks.
     #config.active_record.raise_in_transactional_callbacks = true
     #the line above was giving me a deprecation warning so I commented it out
+    
+     config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
